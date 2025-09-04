e8696aa2d1c00e778bb3375aeebb4c15a1cc3942
Laura Maxwell <laura.maxwell@atorusresearch.com>
2025-09-04 09:02:32 -0400

    add functions to initialize and update packages with global gsm github artifacts

---
+++ R/util-init_gsm_package.R
@@ -0,0 +1,37 @@
+#' Initialize gsm Extension package
+#'
+#' @param strPackageDir path to package directory
+#' @param lDescriptionFields `list` of description fields, passed to [usethis::create_package()]. Default is `list()`.
+#' @param bIncludeWorkflowDir `boolean` argument declaring whether or not to include the `inst/workflow` directory in the root of the package. Default is `TRUE`.
+#'
+init_gsm_package <- function(strPackageDir,
+                             lDescriptionFields = list(),
+                             bIncludeWorkflowDir = TRUE) {
+    if (!dir.exists(package_dir)) {
+        dir.create(package_dir)
+        init_git <- TRUE
+    }
+    usethis::create_package(package_dir,
+                            open = F,
+                            fields = description_fields
+    )
+    withr::with_dir(package_dir, {
+        usethis::use_pkgdown_github_pages()
+        usethis::use_testthat()
+        usethis::use_github_action("check-standard")
+        dir.create("inst")
+        
+        # add gsm-specific GHA and issue template content
+        file.copy(system.file(".github", package = "gsm.utils"),
+                  ".github",
+                  recursive = T
+        )
+        if (include_workflow_dir) {
+            dir.create("inst/workflow")
+            dir.create("inst/workflow/1_mappings")
+            dir.create("inst/workflow/2_metrics")
+            dir.create("inst/workflow/3_reporting")
+            dir.create("inst/workflow/4_modules")
+        }
+    })
+}
---
+++ R/util-update_gsm_package.R
@@ -0,0 +1,62 @@
+#' Update GSM package with global issue templates and GH actions
+#'
+#' @param strPackageDir path to package directory
+#'
+#' @returns NULL
+#' @export
+update_gsm_package <- function(strPackageDir = ".") {
+    if (!dir.exists(package_dir)) {
+        stop("The specified package directory does not exist.")
+    }
+    ##add issue templates
+    add_gsm_issue_templates(strPackageDir = strPackageDir)
+    
+    ##add github actions
+    add_gsm_issue_templates(strPackageDir = strPackageDir)
+}
+
+#' Add GSM issue templates to package
+#'
+#' @param strPackageDir path to package directory
+#' @param overwrite `boolean` argument declaring whether or not to overwrite existing files. Default is `TRUE`.
+#'
+#' @export
+add_gsm_issue_templates <- function(strPackageDir = ".",
+                                    overwrite = TRUE) {
+    if (!dir.exists(paste0(strPackageDir, "/.github/ISSUE_TEMPLATE"))) {
+        dir.create(paste0(strPackageDir, "/.github/ISSUE_TEMPLATE"),
+                   recursive = TRUE
+        )
+    } else if (!overwrite) {
+        stop("The .github/ISSUE_TEMPLATE directory already exists. Set overwrite = TRUE to overwrite it.")
+    }
+    
+    file.copy(system.file(".github/ISSUE_TEMPLATE", package = "gsm.utils"),
+              ".github/ISSUE_TEMPLATE",
+              recursive = T,
+              overwrite = overwrite
+    )
+}
+
+#' Add GSM GitHub Actions to package
+#'
+#' @param strPackageDir path to package directory
+#' @param overwrite `boolean` argument declaring whether or not to overwrite existing files. Default is `TRUE`.
+#'
+#' @export
+add_gsm_actions <- function(strPackageDir = ".",
+                               overwrite = TRUE) {
+    if (!dir.exists(paste0(strPackageDir, "/.github/workflows"))) {
+        dir.create(paste0(strPackageDir, "/.github/workflows"),
+                   recursive = TRUE
+        )
+    } else if (!overwrite) {
+        stop("The .github/workflows directory already exists. Set overwrite = TRUE to overwrite it.")
+    }
+    
+    file.copy(system.file(".github/workflows", package = "gsm.utils"),
+              ".github/workflows",
+              recursive = T,
+              overwrite = overwrite
+    )
+}
